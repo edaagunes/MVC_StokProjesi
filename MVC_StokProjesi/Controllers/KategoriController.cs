226c20e7cf91ff67908b60f358f7189af873fb71
edaagunes <edagunes00@hotmail.com>
2024-09-11 15:32:41 +0300

    Add project files.

---
+++ MVC_StokProjesi/Controllers/KategoriController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_StokProjesi.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MVC_StokProjesi.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+
+        MVCDBSTOKEntities db = new MVCDBSTOKEntities();
+        public ActionResult Index(int sayfa=1)
+        {
+            //var degerler = db.TBL_KATEGORILER.ToList();
+            var degerler=db.TBL_KATEGORILER.ToList().ToPagedList(sayfa,4);
+            return View(degerler);
+        }
+
+        [HttpGet] // sayfayi yukle sadece
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+        [HttpPost] //sayfada degisiklik yapildiginda yap 
+        public ActionResult YeniKategori(TBL_KATEGORILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBL_KATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult SIL(int id)
+        {
+            var kategori = db.TBL_KATEGORILER.Find(id);
+            db.TBL_KATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBL_KATEGORILER.Find(id);
+            return View("KategoriGetir",ktgr);
+        }
+
+        public ActionResult Guncelle(TBL_KATEGORILER p1)
+        {
+            var ktg = db.TBL_KATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD=p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_StokProjesi/Controllers/SatisController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_StokProjesi.Models.Entity;
+
+namespace MVC_StokProjesi.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+
+        MVCDBSTOKEntities db = new MVCDBSTOKEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(TBL_SATISLAR p)
+        {
+            db.TBL_SATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
